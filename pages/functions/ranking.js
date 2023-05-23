ad9dc115207470ef772e5416ccc7e6763feb3830
Nils Bebelaar <nils@bebelaar.net>
2023-05-23 11:17:46 +0200

    Update ranking.js

--- pages/functions/ranking.js
+++ pages/functions/ranking.js
@@ -1,7 +1,7 @@
 export async function onRequest(context) {
     // Create a prepared statement with our query
     const ps = context.env.VIJF_REGIO_DB.prepare('SELECT * from regio_ranking');
-    const data = await ps.first();
+    const data = await ps.all();
   
     return Response.json(data);
   }
